8aecbf8b319ae84750b584b6a24eab602ab85467
unitedtimur <lihomanov.t@inbox.ru>
2022-05-10 20:00:01 +0700

    Add function for gettings prefix of wiki url

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import argparse
 import validators
+import re
+
+
+# Function checks if is url is wiki url
+def is_wiki_url(url):
+    return re.search('(https://).*(wiki).*(/wiki/)', url)
+
+
+# Returns the lang prefix from url
+def get_lang_prefix(url):
+    return re.search(r'(https://)(.*?)(.wiki)', url).group(2)
+
 
 if __name__ == '__main__':
-    argparser = argparse.ArgumentParser(description='Road2Wiki for searching path from A to B with rate limit and depth')
-    argparser.add_argument('--start', type=str, required=True, help='Start page')
-    argparser.add_argument('--finish', type=str, required=True, help='Last page')
-    argparser.add_argument('--rate_limit', type=int, default=10, help='Number of restrictions per minute')
-    argparser.add_argument('--depth', type=int, default=5, help='Depth for searching last page')
-    args = argparser.parse_args()
+    arg_parser = argparse.ArgumentParser(
+        description='Road2Wiki for searching path from A to B with rate limit and depth')
+    arg_parser.add_argument('--start', type=str, required=True, help='Start page')
+    arg_parser.add_argument('--finish', type=str, required=True, help='Last page')
+    arg_parser.add_argument('--rate_limit', type=int, default=10, help='Number of restrictions per minute')
+    arg_parser.add_argument('--depth', type=int, default=5, help='Depth for searching last page')
+    args = arg_parser.parse_args()
 
-    if not validators.url(args.start) or not validators.url(args.finish):
-        print("Not valid link or links")
+    if not validators.url(args.start) or not validators.url(args.finish) or not is_wiki_url(args.start) \
+            or not is_wiki_url(args.finish):
+        print("Valid/s not valid")
         exit(1)
 
-
+    print(get_lang_prefix(args.start), get_lang_prefix(args.finish))
